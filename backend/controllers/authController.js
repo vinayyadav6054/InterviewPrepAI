c422d856476726cbd092aa9fdfb0ef06883ce1ea
vinayyadav6054 <yvinay7271@gmail.com>
2025-07-12 01:24:24 +0530

    Register User Control is completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,6 +13,38 @@ const generateToken = (userId) => {
 //@route   POST /api/auth/register
 //@access  Public
 const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, profileImageUrl} = req.body;
+
+        //Check if user already exists
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        //Hash password
+        const salt = await bcrypt.getSalt(10);
+        const hashedPassword  = await bcrypt.hash(password, salt);
+
+        //Create new user
+        const user = await User.create({
+            name, 
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+        });
+
+        //Return user data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message});
+    }
 };
 
 //@desc      Login user
