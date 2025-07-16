09e875a037425fb0c3b30ba51e103a22f1638a27
vinayyadav6054 <yvinay7271@gmail.com>
2025-07-17 01:02:00 +0530

    Configred and Created AI Apis  successfully

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes"); 
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
+
 const app = express();
 
 //Middleware to handle CORS
@@ -28,8 +31,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestion);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
